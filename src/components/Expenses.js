a44f251d4176a6bfeaa51fa4e1e3018f15e736aa
Justin Kaese <justinkaese@play-force.com.au>
2021-11-21 20:54:42 +1000

    first assignment

---
+++ src/components/Expenses.js
@@ -0,0 +1,19 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses(props) {
+  const expenses = props.expenses.map((expense) => {
+    return (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    );
+  });
+
+  return <div className="expenses">{expenses}</div>;
+}
+
+export default Expenses;
